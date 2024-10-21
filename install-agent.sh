7020c13228b5a38ec266fb9f8a7f61642bff5833
ericnjenga <e.njenga@offensive-security.com>
2024-10-21 05:14:24 +0300

    automated token extraction and double running

--- install-agent.sh
+++ install-agent.sh
@@ -4,25 +4,32 @@
 windows_ip=""
 username=""
 password=""
-elastic_agent_token=""
+operating_system=""
+
+# Define the file where tokens are stored
+token_file="/opt/elk-installer/tokens/enrollment_token.txt" 
+
+# Define the path to the Elastic Agent zip file and commands file
+elastic_zip="/opt/elk-installer/elastic-agent-8.14.3-windows-x86_64.zip"
+commands_file="/opt/elk-installer/commands"
 
 # Function to display usage information
 usage() {
-    echo "Usage: $0 -i <Windows IP> -u <username> -p <password> -t <elastic-agent-token>"
+    echo "Usage: $0 -i <Windows IP> -u <username> -p <password> -o <operating system>"
     echo "  -i : IP address of the Windows machine"
     echo "  -u : Username for Windows machine"
     echo "  -p : Password for Windows machine"
-    echo "  -t : Elastic Agent token"
+    echo "  -o : Operarting System type"
     exit 1
 }
 
 # Parse command-line options
-while getopts ":i:u:p:t:" opt; do
+while getopts ":i:u:p:o:" opt; do
     case $opt in
         i) windows_ip="$OPTARG" ;;
         u) username="$OPTARG" ;;
         p) password="$OPTARG" ;;
-        t) elastic_agent_token="$OPTARG" ;;
+        o) operating_system="$OPTARG" ;;
         \?) echo "Invalid option: -$OPTARG" >&2; usage ;;
         :) echo "Option -$OPTARG requires an argument." >&2; usage ;;
     esac
@@ -33,7 +40,7 @@ done
 tun0_ip=$(/usr/share/kali-themes/xfce4-panel-genmon-vpnip.sh | awk -F '<txt>' '{print $2}' | awk -F '</txt>' '{print $1}')
 
 # Ensure variables are set
-if [ -z "$windows_ip" ] || [ -z "$elastic_agent_token" ] || [ -z "$tun0_ip" ] || [ -z "$username" ] || [ -z "$password" ]; then
+if [ -z "$windows_ip" ] || [ -z "$operating_system" ] || [ -z "$tun0_ip" ] || [ -z "$username" ] || [ -z "$password" ]; then
     echo "Error: Please check your inputs."
     usage
     exit 1
@@ -44,9 +51,27 @@ echo_step() {
     echo "[INFO] $1"
 }
 
-# Define the path to the Elastic Agent zip file and commands file
-elastic_zip="/opt/elk-installer/elastic-agent-8.14.3-windows-x86_64.zip"
-commands_file="/opt/elk-installer/commands"
+# Check if the token file exists
+if [ ! -f "$token_file" ]; then
+    echo "Error: Token file not found at $token_file"
+    exit 1
+fi
+
+# Based on the OS, fetch the relevant token
+case "$operating_system" in
+    win)
+        # Extract the Windows Policy token
+        token=$(grep "Windows Policy" "$token_file" | cut -d ':' -f2)
+        ;;
+    lin)
+        # Extract the Linux Policy token
+        token=$(grep "Linux Policy" "$token_file" | cut -d ':' -f2)
+        ;;
+    *)
+        echo "Invalid operating system option. Use 'win' for Windows or 'lin' for Linux."
+        usage
+        ;;
+esac
 
 # Check if the Elastic Agent zip file exists before downloading
 if [ ! -f "$elastic_zip" ]; then
@@ -78,7 +103,7 @@ fi
 echo_step "Installing the elastic agent..."
 nxc_output=$(nxc smb $windows_ip -u $username -p $password -X "Add-Content -Path C:\\Windows\\System32\\Drivers\\etc\\hosts -Value '$tun0_ip fleet01 elasticsearch'; \
     if (\$?) { Expand-Archive C:\\Windows\\Temp\\elastic-agent-8.14.3-windows-x86_64.zip -DestinationPath C:\\Windows\\Temp -Force; \
-    if (\$?) { C:\\Windows\\Temp\\elastic-agent-8.14.3-windows-x86_64\\elastic-agent.exe install --url=https://fleet01:8220 --enrollment-token=$elastic_agent_token -inf }}" --verbose)
+    if (\$?) { C:\\Windows\\Temp\\elastic-agent-8.14.3-windows-x86_64\\elastic-agent.exe install --url=https://fleet01:8220 --enrollment-token=$token -inf }}" --verbose)
 
 # Check if the output contains the success message
 if echo "$nxc_output" | grep -iq "successfully"; then
--- scripts/token.sh
+++ scripts/token.sh
@@ -2,15 +2,13 @@
 
 # Define the file and token path
 FILE="docker-compose.yml"
-TOKEN_FILE="tokens/token.txt"
+TOKEN_FILE="tokens/enrollment_tokens.txt"
 
 # Run the sed command
-sed -i.bak "s|FLEET_SERVER_SERVICE_TOKEN=.*|FLEET_SERVER_SERVICE_TOKEN=$(cat $TOKEN_FILE)|" $FILE
+sed -i.bak "s|FLEET_SERVER_SERVICE_TOKEN=.*|FLEET_SERVER_SERVICE_TOKEN=$(grep "fleet-service_token" "$TOKEN_FILE" | cut -d ':' -f2)|" $FILE
 
 # Check if the sed command was successful
-if [ $? -eq 0 ]; then
-  echo "The environment variable FLEET_SERVER_SERVICE_TOKEN was successfully updated in $FILE."
-  
+if [ $? -eq 0 ]; then  
   # Remove the .bak file
   rm "${FILE}.bak"
 else
