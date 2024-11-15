f99d16dc1b3f6b6a268e3aa5196a26b62a9a4561
ericnjenga <e.njenga@offensive-security.com>
2024-11-15 16:54:16 +0300

    minor change

--- setup.sh
+++ setup.sh
@@ -252,7 +252,7 @@ configure_aliases() {
     local aliases=(
         "alias elk-start='docker compose -f ${compose_path} start elasticsearch kibana elastic-agent && echo \"\nAccess the Elastic SIEM at: http://localhost:5601\"'"
         "alias elk-stop='docker compose -f ${compose_path} stop'"
-        "alias elk-reset='(cd ${ELK_PATH} && sudo git reset --hard &>/dev/null && sudo git pull &>/dev/null && sudo ./setup.sh'"
+        "alias elk-reset='(cd ${ELK_PATH} && sudo bash -c \"git reset --hard &>/dev/null && git pull &>/dev/null && ./setup.sh\"'"
     )
 
     # Check if aliases already exist
