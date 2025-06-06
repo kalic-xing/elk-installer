39eae343a0b037816d0913d86b1b8d4a73617f69
ericnjenga <e.njenga@offensive-security.com>
2025-06-06 10:35:33 +0300

    changed the version

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ readonly DOCKER_SOURCE_LIST="/etc/apt/sources.list.d/docker.list"
 
 # Default configuration
 DEFAULT_PASSWORD="lablab"
-DEFAULT_VERSION="9.0.1"
+DEFAULT_VERSION="8.14.1"
 COMPOSE_FILE="${ELK_PATH}/docker-compose.yml"
 
 # Create a Random Kibana Password
